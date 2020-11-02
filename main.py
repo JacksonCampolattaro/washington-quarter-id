61788330641df12d86f9ee996fdd843360e00e6c
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-02 10:56:18 -0500

    Add a test image, convert it to grayscale and display it

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-
 import cv2
 
 if __name__ == '__main__':
-    print("Hello, world!")
+
+    image = cv2.imread("data/test.jpg")
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("output", grayscale)
+    cv2.waitKey(0)
