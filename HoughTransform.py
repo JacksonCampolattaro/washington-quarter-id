e65ef1086f69d63bf3eda7a4c991993a1dd9d7a4
oranosskyman <jnalchajian@gmail.com>
2020-11-04 20:23:18 -0500

    Circles Found - quarters are blurry blobs, but the circles HAVE BEEN FOUND

--- HoughTransform.py
+++ HoughTransform.py
@@ -4,14 +4,14 @@ import cv2
 
 
 img = cv2.imread("image/TestQuarters.jpg", 0)
-img = cv2.medianBlur(img, 7)
+img = cv2.medianBlur(img, 25)
 width = int(img.shape[1] * 0.2)
 height = int(img.shape[0] * 0.2)
 dim = (width, height)
 zimg = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 cimg = cv2.cvtColor(zimg, cv2.COLOR_GRAY2BGR)
 
-circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, 300, param1=30, param2=30, minRadius=0, maxRadius=0)
+circles = cv2.HoughCircles(zimg, cv2.HOUGH_GRADIENT, 1, 50)
 
 circles = np.uint16(np.around(circles))
 for i in circles[0, :]:
