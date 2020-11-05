b08b6872db5b36a02603dea1d327819a94ff0cf4
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-05 12:48:14 -0500

    Move circle-finding code to its own function

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ from datetime import datetime
 import numpy as np
 import cv2
 
-if __name__ == '__main__':
 
-    # Load an image
-    image = cv2.imread("data/test.jpg")
-    print('Loaded image of size {}x{}'.format(image.shape[0], image.shape[1]))
-
+def find_circles(image):
     # Convert the image to grayscale
     grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -19,13 +15,10 @@ if __name__ == '__main__':
     ret, binary = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY)
     cv2.imwrite("binary.jpg", binary)
 
-    # Create an output image we can draw on
-    output = image.copy()
-
     # Find the circles in the image
-    coinRadius = 300
+    coin_radius = 300
     circles = cv2.HoughCircles(binary, cv2.HOUGH_GRADIENT, 2,
-                               coinRadius * 2,
+                               coin_radius * 2,
                                # param1=100,
                                # param2=0.8,
                                # minRadius=coinRadius - 100,
@@ -39,8 +32,22 @@ if __name__ == '__main__':
     # Convert circle data to integers
     circles = np.round(circles[0, :]).astype("int")
 
+    return circles
+
+
+if __name__ == '__main__':
+
+    # Load an image
+    image = cv2.imread("data/test.jpg")
+    print('Loaded image of size {}x{}'.format(image.shape[0], image.shape[1]))
+
+    # Create an output image we can draw on
+    output = image.copy()
+
+    circles_found = find_circles(image)
+
     # Iterate over each of the circles found
-    for (x, y, r) in circles:
+    for (x, y, r) in circles_found:
         # Draw a circle which outlines that one
         cv2.circle(output, (x, y), r, (0, 255, 0), 2)
 
