8386a6be581e9e022f3fc5c2144f4ce644df8d1b
Jackson Campolattaro <jackcamp@vt.edu>
2021-01-20 13:14:52 -0500

    Update camera_test.py to use capture() and create files named after test conditions

--- camera_test.py
+++ camera_test.py
@@ -3,8 +3,21 @@ import cv2
 
 
 def main():
+
+    rel_angle = 180
+    l1_angle = 45
+    l2_angle = 45
+    l1_distance = 35
+    l2_distance = 35
+
     # Save the image
-    cv2.imwrite("test.png", capture())
+    cv2.imwrite("data/RelativeAngle={}deg_VerticalAngles={},{}deg_Distances={},{}cm.png".format(
+        rel_angle,
+        l1_angle,
+        l2_angle,
+        l1_distance,
+        l2_distance
+    ), capture())
 
 
 if __name__ == '__main__':
