c0d352f138131d1b38f8ef091495b6d7de3f5dcc
oranosskyman <jnalchajian@gmail.com>
2021-02-03 15:27:05 -0500

    added a contrast code and edited the crop code to use the new contrasted image

--- Crop.py
+++ Crop.py
@@ -3,10 +3,10 @@ import numpy as np
 import cv2
 # Opens a image in RGB mode
 #im = Image.open(r"C:\Users\Admin\Pictures\geeks.png")
-img = cv2.imread("image/SingleCoinTest.png", 0)
+img = cv2.imread("image/contrast.jpg", 0)
 width = int(img.shape[1])
 height = int(img.shape[0])
-w1 = int(width*.30)
+w1 = int(width*.29)
 w2 = int(width*.68)
 h1 = int(height*.83)
 h2 = int(height*.96)
