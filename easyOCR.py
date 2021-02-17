e72233dbda77a46bf5ad2a852e447381637fd263
oranosskyman <jnalchajian@gmail.com>
2021-02-17 13:51:45 -0500

    Wont let me pull unless i commit this

---
+++ easyOCR.py
@@ -0,0 +1,10 @@
+import easyocr
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+IMAGE_PATH = 'Image/Date.png'
+
+reader = easyocr.Reader(['en'], gpu=False)
+result = reader.readtext(IMAGE_PATH,'greedy',5,1,0,"0123456789")
+print(result)
