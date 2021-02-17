2bfa83dab060b53f92d22890dfa653ac7a67c113
pas7a <pasha@vt.edu>
2021-02-17 10:20:40 -0500

    Incorporate Fahad's Tesseract and EasyOCR code

---
+++ test2.py
@@ -0,0 +1,24 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\pashf\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+
+
+img = cv2.imread('Image/htest.jpg')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#print(pytesseract.image_to_string(img))
+#print(pytesseract.image_to_boxes(img))
+
+###Character Detection####
+
+hImg,wImg,_ = img.shape
+boxes = pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+    print(b)
+    b = b.split(' ')
+    print(b)
+    x,y,w,h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
+
+cv2.imshow('Result',img)
+cv2.waitKey(0)
