c7ef2ea1f7abff823121c536e71c3ee5ba770f10
Jackson Campolattaro <jackcamp@vt.edu>
2021-03-08 16:16:14 -0500

    Implement batch based circle-finding unit test

--- tests/test_coin_isolation.py
+++ tests/test_coin_isolation.py
@@ -8,37 +8,37 @@ from quarterid import coin_isolation
 
 class TestCoinIsolation(unittest.TestCase):
 
-    def SetUp(self):
-
-        images = []
+    def setUp(self):
+        self.images = []
 
         # These tests will use all the images with annotated positions
         path = os.path.join(os.path.dirname(__file__), "data/positions")
 
         # We're going to try every image
         for filename in glob.glob(os.path.join(path, "*.png")):
-            with cv2.imread(filename, cv2.IMREAD_GRAYSCALE) as image:
-                images.append(image)
-                # TODO set image position
 
-        return
+            # Load the image
+            image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+            # Get the expected location from the path
+            name = os.path.splitext(os.path.basename(filename))[0]
+            location = [int(x) for x in name.split(',')]
+
+            # Add this pair to the list
+            self.images.append((image, location))
 
     def test_find_circles(self):
 
-        # Open the file containing our image
-        image = cv2.imread(
-            "data/positions/2147,1121,730.png",
-            cv2.IMREAD_GRAYSCALE
-        )
+        for (image, (known_x, known_y, known_r)) in self.images:
 
-        # Use our function to find the locations
-        locations = coin_isolation.find_circles(image, pix_radius=745)
+            # Use our function to find the locations
+            locations = coin_isolation.find_circles(image, pix_radius=745)
 
-        # There should only be one coin
-        self.assertEqual(1, len(locations))
+            # There should only be one coin
+            self.assertEqual(1, len(locations))
 
-        # The location of the coin should be pretty close to a known correct location
-        (x, y, r) = locations[0]
-        self.assertAlmostEqual(2161, x, delta=5)
-        self.assertAlmostEqual(1131, y, delta=5)
-        self.assertAlmostEqual(745, r, delta=10, msg="Incorrect radius!")
+            # The location of the coin should be pretty close to the known correct location
+            (x, y, r) = locations[0]
+            self.assertAlmostEqual(known_x, x, delta=5, msg="Incorrect x")
+            self.assertAlmostEqual(known_y, y, delta=5, msg="Incorrect y")
+            self.assertAlmostEqual(known_r, r, delta=10, msg="Incorrect radius!")
