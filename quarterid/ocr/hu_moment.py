85cf486b55e0a7c3374a6a4901b43469ffb27e67
Jackson Campolattaro <jackcamp@vt.edu>
2021-03-30 15:04:43 -0400

    Add the skeleton of the hu-moment implementation, inspired by Justin's code

--- quarterid/ocr/hu_moment.py
+++ quarterid/ocr/hu_moment.py
@@ -1,8 +1,5 @@
 import cv2
-import easyocr
 import logging
-
-import numpy as np
 
 from quarterid import image_logging, preprocessing
 
@@ -10,4 +7,20 @@ logger = logging.getLogger(__name__)
 
 
 def read_character(character_image, allowlist, default):
-    return '9', 1
+
+    # Load our templates, if we don't already have them
+    if not hasattr(read_character, 'templates'):
+        # TODO
+
+        # Read our template images into a map (key is the character they represent)
+
+        return
+
+    # Compare the contour of the input image to that of each template
+
+    # Chosen character is the one with the least "distance"
+
+    # Smaller distances indicate lower confidence
+
+    # Return the character, and the confidence
+    return '9', 0.1
